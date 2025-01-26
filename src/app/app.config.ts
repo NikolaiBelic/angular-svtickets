17eaf3ddef9ac5a8402d7b40fa128541b69197e6
Álvaro Nicolás Climent <akralan1414@gmail.com>
2025-01-26 16:58:14 +0100

    anamtions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { baseUrlInterceptor } from './shared/interceptors/base-url.interceptor';
 import { authInterceptor } from './shared/interceptors/auth.interceptor';
 import { provideGoogleId } from './auth/google-login/google-login.config';
 import { provideFacebookId } from './auth/facebook-login/facebook-login.config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 import { routes } from './app.routes';
@@ -15,7 +16,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)), 
     provideHttpClient(withInterceptors([baseUrlInterceptor, authInterceptor])),
     provideGoogleId('746820501392-oalflicqch2kuc12s8rclb5rf7b1fist.apps.googleusercontent.com'), 
-    provideFacebookId('2024262238091054', 'v21.0')
+    provideFacebookId('2024262238091054', 'v21.0'), 
+    provideAnimationsAsync()
   ]
 };
 //provideZoneChangeDetection({ eventCoalescing: true })
